23828163d4831724c911b3f46fbf25c921b62b55
Gagan0322 <sin12455@sheridancollege.ca>
2023-01-31 13:14:30 -0500

    i am done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -63,6 +63,7 @@ public class CardTrick
      * replace this information with your own.
      * @author Paul Bonenfant Jan 2022
      */
+        //i am done 
     private static void printInfo()
     {
     
